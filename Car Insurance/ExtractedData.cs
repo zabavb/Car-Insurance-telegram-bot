4a04773e68cc56439ad1a94b8d69a1a91f0cbbf0
zabavb <bilonizkavik@gmail.com>
2025-06-15 19:52:24 +0200

    Implemented model ExtractedData for retrieving data from Passport scan, provided by Mindee.

--- Car Insurance/ExtractedData.cs
+++ Car Insurance/ExtractedData.cs
@@ -2,5 +2,8 @@
 
 public class ExtractedData
 {
-    
+    public string PassportName { get; set; } = string.Empty;
+    public string PassportSurname { get; set; } = string.Empty;
+    public string PassportId { get; set; } = string.Empty;
+    public string VehicleId { get; set; } = string.Empty;
 }
